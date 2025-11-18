0f192423d5979616067c41dc21b122487809cd26
sempijja <ssempijacharz@gmail.com>
2025-11-18 13:38:19 +0300

    - PWA rcih format setup - SEO setup - Revamped help modal UI

--- vite.config.js
+++ vite.config.js
@@ -42,25 +42,109 @@ export default defineConfig({
         name: 'Canvas',
         short_name: 'Canvas',
         description: 'An interactive web map application for visualizing geographical data.',
-        start_url: '.',
+        start_url: '/',
+        orientation: 'portrait',
+        prefer_related_applications: false,
         display: 'standalone',
         background_color: '#ffffff',
         theme_color: '#ffffff',
         scope: '/',
-        icons: [
+  icons: [
+          {
+            src: '/favicons.ico/android-icon-36x36.png',
+            sizes: '36x36',
+            type: 'image/png',
+            density: '0.75'
+          },
+          {
+            src: '/favicons.ico/android-icon-48x48.png',
+            sizes: '48x48',
+            type: 'image/png',
+            density: '1.0'
+          },
+          {
+            src: '/favicons.ico/android-icon-72x72.png',
+            sizes: '72x72',
+            type: 'image/png',
+            density: '1.5'
+          },
+          {
+            src: '/favicons.ico/android-icon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+            density: '2.0'
+          },
+          {
+            src: '/favicons.ico/android-icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+            density: '3.0'
+          },
           {
             src: '/favicons.ico/android-icon-192x192.png',
             sizes: '192x192',
             type: 'image/png',
-            purpose: 'any maskable'
+             density: '4.0'
+            }
+          ],
+        screenshots: [
+          {
+            src: '/src/assets/candidate photos/localhost_4173_(iPhone 14 Pro Max).png',
+            sizes: '1290x2796',
+            type: 'image/png',
+            label: 'localhost_4173 iPhone 14 Pro Max',
+            form_factor: 'narrow'
           },
           {
-            src: '/favicons.ico/android-icon-512x512.png',
-            sizes: '512x512',
+            src: '/src/assets/candidate photos/localhost_4173_(iPhone 14 Pro Max).png',
+            sizes: '1290x2796',
             type: 'image/png',
-            purpose: 'any maskable'
+            label: 'localhost_4173 iPhone 14 Pro Max (web)',
+            form_factor: 'wide'
+          },
+          {
+            src: '/src/assets/candidate photos/localhost_417_(iPhone 14 Pro Max).png',
+            sizes: '1290x2796',
+            type: 'image/png',
+            label: 'localhost_417 iPhone 14 Pro Max',
+            form_factor: 'narrow'
+          },
+          {
+            src: '/src/assets/candidate photos/localhost_417_(iPhone 14 Pro Max).png',
+            sizes: '1290x2796',
+            type: 'image/png',
+            label: 'localhost_417 iPhone 14 Pro Max (web)',
+            form_factor: 'wide'
+          },
+          {
+            src: '/src/assets/candidate photos/localhst_4173_(iPhone 14 Pro Max).png',
+            sizes: '1290x2796',
+            type: 'image/png',
+            label: 'localhst_4173 iPhone 14 Pro Max',
+            form_factor: 'narrow'
+          },
+          {
+            src: '/src/assets/candidate photos/localhst_4173_(iPhone 14 Pro Max).png',
+            sizes: '1290x2796',
+            type: 'image/png',
+            label: 'localhst_4173 iPhone 14 Pro Max (web)',
+            form_factor: 'wide'
+          },
+          {
+            src: '/src/assets/candidate photos/lolhost_4173_(iPhone 14 Pro Max).png',
+            sizes: '1290x2796',
+            type: 'image/png',
+            label: 'lolhost_4173 iPhone 14 Pro Max',
+            form_factor: 'narrow'
+          },
+          {
+            src: '/src/assets/candidate photos/lolhost_4173_(iPhone 14 Pro Max).png',
+            sizes: '1290x2796',
+            type: 'image/png',
+            label: 'lolhost_4173 iPhone 14 Pro Max (web)',
+            form_factor: 'wide'
           }
-        ]
+        ],
       }
     })
   ],
